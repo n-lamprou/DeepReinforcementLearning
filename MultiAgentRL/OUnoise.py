a7bde3ba9547545cbf77d7fd26e39e884de321c4
Nik Lamprou <nick_labrou@hotmail.com>
2018-11-20 09:47:02 +0000

    Add OUNoise class
    Addition of class to add correlated noise to action space via the OU process

---
+++ MultiAgentRL/OUnoise.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+import random
+import copy
+    
+    
+class OUNoise:
+    """
+    Ornstein-Uhlenbeck process.
+    """
+
+    def __init__(self, size, seed, scale=0.1, mu=0., theta=0.15, sigma=0.2):
+        """
+        Initialize parameters and noise process
+        """
+        self.mu = mu * np.ones(size)
+        self.scale = scale
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """
+        Reset the internal state (= noise) to mean (mu)
+        """
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """
+        Update internal state and return it as a noise sample
+        """
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return torch.tensor(self.state * self.scale).float()
